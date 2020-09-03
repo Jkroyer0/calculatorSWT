649599e6f42b14472e3e8585ab7a74861d597bed
Joachim <Kroyer.joachim@gmail.com>
2020-09-03 13:25:00 +0200

    Add project files.

---
+++ calculatorSWT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace calculatorSWT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Hello World!");
+
+            Calculator calc = new Calculator();
+
+            double addRes = calc.Add(5, 6);
+            double subRes = calc.Subtract(8, 9);
+            double mulRes = calc.Multiply(5, 2);
+            double powRes = calc.Power(2, 6);
+
+            Console.WriteLine("5+6 = ", addRes);
+            Console.WriteLine("8-9 = ", subRes);
+            Console.WriteLine("5*2 = ", mulRes);
+            Console.WriteLine("2 raised to 6 = ", powRes);
+        }
+    }
+}
---
+++ calculatorSWTtest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using calculatorSWT;
+using NUnit.Framework;
+
+namespace calculatorSWTtest
+{
+    [TestFixture]
+    public class calculatorTest
+    {
+        [Test]
+        public void AddItems()
+        {
+            Calculator uut = new Calculator();
+
+            Assert.That(uut.Add(4,5), Is.EqualTo(9));
+        }
+    }
+}
